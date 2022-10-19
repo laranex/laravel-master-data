cff0db834b27fb0b390e733da23ebfa46932229f
Soe Thura <thixpin@gmail.com>
2022-10-19 23:28:15 +0630

    mod: columns properties added

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,8 +23,53 @@ class User extends Authenticatable
         'password',
     ];
 
+    /**
+     * The attributes that should be hidden in master data.
+     *
+     * @var array<int, string>
+     */
     public $unpublished = [
         'password',
+    ];
+
+    /**
+     * The attributes that should be sortable in master data.
+     *
+     * @var array<int, string>
+     */
+    public $sortable = [
+        'name',
+        'email',
+        'age',
+    ];
+
+    /**
+     * The attributes that should be searchable in master data.
+     *
+     * @var array<int, string>
+     */
+    public $searchable = [
+        'name',
+        'email',
+    ];
+
+    /**
+     * The attributes that should be filterable in master data.
+     *
+     * @var array<string, array<string, string>>
+     */
+    public $filterable = [
+        'created_at' => [
+            'type' => 'date',
+            'format' => 'YYYY-MM-DD',
+        ],
+        'name' => [
+            'type' => 'select',
+            'options' => [
+                'male',
+                'female',
+            ],
+        ],
     ];
 
     /**
--- packages/laranex/laravel-master-data/src/Helpers/ModelHelper.php
+++ packages/laranex/laravel-master-data/src/Helpers/ModelHelper.php
@@ -18,17 +18,6 @@ class ModelHelper
         return new $model;
     }
 
-    public static function getModelColumns($slug)
-    {
-        $model = self::getModel($slug);
-        $unpublished = isset($model->unpublished) ? $model->unpublished : [];
-        $hidden = array_merge($model->getHidden(), $unpublished);
-        $columns = $model->getConnection()->getSchemaBuilder()->getColumnListing($model->getTable());
-        $columns = array_values(array_diff($columns, $hidden));
-
-        return $columns;
-    }
-
     public static function getModelList()
     {
         $prefix = config('laravel-master-data.prefix');
@@ -45,5 +34,38 @@ class ModelHelper
         }
 
         return $list;
+    }
+
+    public static function getModelColumns($slug)
+    {
+        $model = self::getModel($slug);
+        $unpublished = isset($model->unpublished) ? $model->unpublished : [];
+        $hidden = array_merge($model->getHidden(), $unpublished);
+        $columns = $model->getConnection()->getSchemaBuilder()->getColumnListing($model->getTable());
+        $columns = array_values(array_diff($columns, $hidden));
+
+        $searchable = isset($model->searchable) ? $model->searchable : [];
+        $filterable = isset($model->filterable) ? $model->filterable : [];
+        $sortable = isset($model->sortable) ? $model->sortable : $columns;
+
+        $defaultFilter = [
+            'type' => 'text',
+            'options' => [],
+        ];
+
+        $columns = collect($columns)->map(function ($column) use ($searchable, $filterable, $sortable, $defaultFilter) {
+            $isFilterable = in_array($column, array_keys($filterable));
+
+            return [
+                $column => [
+                    'sortable' => in_array($column, $sortable),
+                    'searchable' => in_array($column, $searchable),
+                    'filterable' => $isFilterable,
+                    'filter' => $isFilterable ? array_merge($defaultFilter, $filterable[$column] ?? []) : null,
+                ],
+            ];
+        });
+
+        return $columns;
     }
 }
